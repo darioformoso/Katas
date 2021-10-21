4c6b957636e2819b67ce95df8a51a23a2f328076
darioformoso <88165956+darioformoso@users.noreply.github.com>
2021-10-20 22:25:32 -0400

    kata

---
+++ 7-kyu/ReturnTheMissingElement.js
@@ -0,0 +1,20 @@
+//Fellow code warrior, we need your help! We seem to have lost one of our sequence elements, and we need your help to retrieve it!
+
+//Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
+
+//Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
+
+
+//Solution
+function getMissingElement(superImportantArray){
+    superImportantArray.sort()
+   
+   
+    for(i=0;i<superImportantArray.length;i++){
+      if(superImportantArray[i]!== i){
+        return i
+       
+      } 
+       
+    }
+  }
