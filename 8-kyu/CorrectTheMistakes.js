4026fc0d1442338523741aa2b58873d9d65e28eb
darioformoso <88165956+darioformoso@users.noreply.github.com>
2021-11-22 16:18:10 -0500

    katas

--- 8-kyu/CorrectTheMistakes.js
+++ 8-kyu/CorrectTheMistakes.js
@@ -1,4 +1,8 @@
+//Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
 
+//When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+//Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
 
 
 //Solution
