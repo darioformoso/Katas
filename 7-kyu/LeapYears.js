439b8fa06c6e13d6c1d7f712b330c44b21222af0
darioformoso <88165956+darioformoso@users.noreply.github.com>
2021-12-08 14:45:21 -0500

    katas

---
+++ 7-kyu/LeapYears.js
@@ -0,0 +1,14 @@
+//Solution
+function isLeapYear(year) {
+    if(year%4===0){
+      if(year%400===0){
+        return true 
+      }else if(year%100===0){
+        return false 
+      }
+      else 
+        return true 
+    }
+     else 
+      return false
+   }
