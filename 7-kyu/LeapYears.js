f4d9496e3336c26adfb7b334015cb5fe102597b3
darioformoso <88165956+darioformoso@users.noreply.github.com>
2021-12-08 14:46:21 -0500

    katas

--- 7-kyu/LeapYears.js
+++ 7-kyu/LeapYears.js
@@ -1,3 +1,9 @@
+//n this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+
+//years divisible by 4 are leap years
+//but years divisible by 100 are not leap years
+//but years divisible by 400 are leap years
+
 //Solution
 function isLeapYear(year) {
     if(year%4===0){
