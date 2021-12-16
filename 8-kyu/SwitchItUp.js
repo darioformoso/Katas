7407b7f6221ec6c1733904fbefaf44b438174c8e
darioformoso <88165956+darioformoso@users.noreply.github.com>
2021-12-16 15:18:47 -0500

    katas

---
+++ 8-kyu/SwitchItUp.js
@@ -0,0 +1,34 @@
+//Solution
+function switchItUp(number){
+    switch (number){
+        case 0:
+        return "Zero"
+        break;
+        case 1:
+        return "One";
+        break;
+        case 2: 
+        return "Two";
+        break;
+        case 3:
+        return "Three"
+        break;
+        case 4:
+        return "Four";
+        break;
+        case 5:
+        return "Five";
+        case 6:
+        return "Six";
+        case 7:
+        return "Seven";
+        break;
+        case 8:
+        return "Eight"
+        break;
+        case 9:
+        return "Nine";
+        break;
+        
+    }
+    }
