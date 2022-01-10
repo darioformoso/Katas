2a99fcbc3cd67d1d5855d729789f43deec7c18fe
darioformoso <88165956+darioformoso@users.noreply.github.com>
2022-01-10 17:08:27 -0500

    katas

--- 7-kyu/DifferenceBetweenYears.js
+++ 7-kyu/DifferenceBetweenYears.js
@@ -1,3 +1,5 @@
+//Write a function that receives two strings as parameter. This strings are in the following format of date: YYYY/MM/DD. Your job is: Take the years and calculate the difference between them.
+
 //Solution
 var howManyYears = function(date1, date2){
     date1=date1.split("/")
