46300b64e89b6b23f68afe4ddaa4e820e95e7839
darioformoso <88165956+darioformoso@users.noreply.github.com>
2022-01-10 16:51:32 -0500

    katas

--- 7-kyu/SmallEnough.js
+++ 7-kyu/SmallEnough.js
@@ -1,3 +1,6 @@
+//You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+
 //Solution
 function smallEnough(a, limit){
     let tester=true
