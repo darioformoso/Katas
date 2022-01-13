02aee3f7569407e87baa4c255fff66c1292c5b90
darioformoso <88165956+darioformoso@users.noreply.github.com>
2022-01-13 15:21:49 -0500

    katas

--- 7-kyu/ParseNiceIntFromCharProblem.js
+++ 7-kyu/ParseNiceIntFromCharProblem.js
@@ -1,3 +1,6 @@
+//Assume test input string always valid and may look like "1 year old" or "5 years old", etc.. The first char is number only.
+
+
 //SOlution
 function getAge(inputString){
     inputString=inputString.split(" ")
