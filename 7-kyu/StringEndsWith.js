3ad257237745d3025c5c5d80fb62b4f3f6ba56ff
darioformoso <88165956+darioformoso@users.noreply.github.com>
2022-01-14 22:24:12 -0500

    katas

--- 7-kyu/StringEndsWith.js
+++ 7-kyu/StringEndsWith.js
@@ -1,3 +1,5 @@
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
 //Solution
 function solution(str, ending){
     str=str.split("").reverse()
