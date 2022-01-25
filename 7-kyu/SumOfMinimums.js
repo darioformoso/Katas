e3220f0daa9b6349fe94be19961444a08b448199
darioformoso <88165956+darioformoso@users.noreply.github.com>
2022-01-25 15:38:47 -0500

    katas

--- 7-kyu/SumOfMinimums.js
+++ 7-kyu/SumOfMinimums.js
@@ -1,3 +1,5 @@
+//Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
 //Solution
 function sumOfMinimums(arr) {
     let result =0;
